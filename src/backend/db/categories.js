eec50f54a7d0931728bb342c3cb9531e7ed855b2
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-03-29 16:48:04 +0530

    assets and refactor

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -1,4 +1,8 @@
 import { v4 as uuid } from "uuid";
+import Comedy from "../../Assets/Images/Comedy.jpg";
+import Sports from "../../Assets/Images/Sports.jpg";
+import Podcast from "../../Assets/Images/Podcast.jpg";
+import Gaming from "../../Assets/Images/Gaming.jpg";
 
 /**
  * Category Database can be added here.
@@ -8,20 +12,22 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "Computer Programming",
-    description:
-      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
+    categoryName: "Comedy",
+    image:Comedy,
   },
   {
     _id: uuid(),
-    categoryName: "Frontend Development",
-    description:
-      "Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application",
+    categoryName: "Sports",
+    image:Sports,
   },
   {
     _id: uuid(),
-    categoryName: "Backend Development",
-    description:
-      "Backend Development is also known as server-side development. It is everything that the users don't see and contains behind-the-scenes activities that occur when performing any action on a website.",
+    categoryName: "Podcast",
+    image: Podcast,
   },
+  {
+    _id: uuid(),
+    categoryName: "Gaming",
+    image: Gaming,
+  }
 ];
--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,95 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "Tqsz6fjvhZM",
+    category: "Comedy",
+    title: "Hostel - Stand Up Comedy",
+    creator: "Anubhav Bassi",
     description:
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    thumbnail:"https://img.youtube.com/vi/Tqsz6fjvhZM/maxresdefault.jpg",
+    logo:""
+    
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "Xc5n49aEhwc",
+    category: "Comedy",
+    title: "Delhi Metro",
+    creator: "Aakash Gupta",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    thumbnail: "https://img.youtube.com/vi/Xc5n49aEhwc/maxresdefault.jpg",
+  },
+  {
+    _id: "9-oCDFtqnUo",
+    category: "Comedy",
+    title: "Daughter",
+    creator: "Gaurav Gupta",
+    description:
+    "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    thumbnail: "https://img.youtube.com/vi/9-oCDFtqnUo/maxresdefault.jpg",
+  },
+  {
+    _id: "zsdYLTQ74W4",
+    category: "Comedy",
+    title: "Gaurav Kapoor | No Ball | Stand Up Comedy 2019",
+    creator: "Gaurav Kapoor",
+    description:
+    "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    thumbnail: "https://img.youtube.com/vi/zsdYLTQ74W4/maxresdefault.jpg",
+  },
+  {
+    _id: "E16WhXcIghM",
+    category: "Comedy",
+    title: "Team Animals",
+    creator: "Abhishek Upmanyu",
+    description:
+    "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    thumbnail: "https://img.youtube.com/vi/E16WhXcIghM/maxresdefault.jpg",
+  },
+  {
+    _id: "7vZx6FPM--c",
+    category: "Gaming",
+    title: "The New Brimstone",
+    creator: "Shroud",
+    description:
+    "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    thumbnail: "https://img.youtube.com/vi/7vZx6FPM--c/maxresdefault.jpg",
+  },
+  {
+    _id: "NA7i1K2jXs4",
+    category: "Gaming",
+    title: "New Call of Duty",
+    creator: "chocoTaco",
+    description:
+    "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    thumbnail: "https://img.youtube.com/vi/NA7i1K2jXs4/maxresdefault.jpg",
+  },
+  {
+    _id: "9JPGAkIu3bU",
+    category: "Gaming",
+    title: "54 Kills in Fortnite Squads",
+    creator: "Tfue",
+    description:
+    "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    thumbnail: "https://img.youtube.com/vi/9JPGAkIu3bU/maxresdefault.jpg",
+  },
+  {
+    _id: "4SL8XgDkaX8",
+    category: "Gaming",
+    title: "I Have A Fortnite Skin",
+    creator: "Ninja",
+    description:
+    "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    thumbnail: "https://img.youtube.com/vi/4SL8XgDkaX8/maxresdefault.jpg",
+  },
+  {
+    _id: "Xo_DHUpqDeI",
+    category: "Gaming",
+    title: "The Warzone Solo game",
+    creator: "DrDisRespect",
+    description:
+    "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    thumbnail: "https://img.youtube.com/vi/Xo_DHUpqDeI/maxresdefault.jpg",
   },
 ];
