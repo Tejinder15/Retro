2c0fc333a23a127de11324421a367cd596d5807b
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-04-01 23:31:35 +0530

    refactored code

--- src/Context/index.js
+++ src/Context/index.js
@@ -1,3 +1,4 @@
 export {AuthProvider,useAuth} from "./AuthContext/auth-context";
 export {WatchLaterProvider,useWatchLater} from "./WatchLaterContext/watchlater-context";
 export {HistoryProvider,useHistory} from "./HistoryContext/history-context";
+export {LikeProvider,useLike} from "./LikeContext/like-context";
