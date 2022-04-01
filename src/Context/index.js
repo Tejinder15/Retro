0bddc718b0a05d0ab9d0510e1ec2fc02f2bb28f8
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-04-01 19:27:19 +0530

    made context export simpler

---
+++ src/Context/index.js
@@ -0,0 +1,3 @@
+export {AuthProvider,useAuth} from "./AuthContext/auth-context";
+export {WatchLaterProvider,useWatchLater} from "./WatchLaterContext/watchlater-context";
+export {HistoryProvider,useHistory} from "./HistoryContext/history-context";
