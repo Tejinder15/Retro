560d65af29704e21d46fe366de883b3031651862
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-04-01 19:23:27 +0530

    made export for utils

---
+++ src/Utils/index.js
@@ -0,0 +1,6 @@
+export {addToHistory} from "./addToHistory";
+export {addToWatchlater} from "./addToWatchlater";
+export {getHistory} from "./getHistory";
+export {getWatchLater} from "./getWatchLater";
+export {removeFromHistory} from "./removeFromHistory";
+export {removeFromWatchLater} from "./removeFromWatchLater";
