329222988a6902d3fa04ba52a5eb6e69cd15bd63
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-04-01 23:29:50 +0530

    refactored Pages

--- src/Pages/Liked/Liked.jsx
+++ src/Pages/Liked/Liked.jsx
@@ -1,13 +1,42 @@
 import LikeCard from "../../Components/LikeCard/LikeCard";
 import Navbar from "../../Components/Navbar/Navbar";
 import Sidebar from "../../Components/Sidebar/Sidebar";
+import { useEffect } from "react";
+import { useLike, useAuth } from "../../Context";
+import { getLike, removeFromLike } from "../../Utils/";
 
 const Liked = () => {
+  const { LikeState, LikeDispatch } = useLike();
+  const { authState } = useAuth();
+  const { token } = authState;
+  const { like } = LikeState;
+
+  const removeLikeHandler = async (videoid) => {
+    removeFromLike(videoid, token, LikeDispatch);
+  };
+
+  useEffect(() => getLike(token, LikeDispatch), []);
   return (
     <div>
       <Navbar />
-      <div className="liked_container">
-        <LikeCard />
+      <div className="video_main_container">
+        <h2 className="page_title">Liked</h2>
+        <div className="liked_container">
+          {like.length !== 0 ? (
+            like.map((item) => (
+              <LikeCard
+                key={item._id}
+                image={item.thumbnail}
+                title={item.title}
+                creator={item.creator}
+                vidId={item._id}
+                removefromlike={removeLikeHandler}
+              />
+            ))
+          ) : (
+            <h2 className="page_title">No Videos Liked😥.</h2>
+          )}
+        </div>
       </div>
       <Sidebar />
     </div>
