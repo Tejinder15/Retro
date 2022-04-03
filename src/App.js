6403f02c184ad8e800486a8d529d674338ea6cb7
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-04-03 19:07:12 +0530

    added provider and page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Home,Login,Signup,Videos,Liked,Playlist, Watch,WatchLater,History} from "./Pages";
+import {Home,Login,Signup,Videos,Liked,Playlist, Watch,WatchLater,History,SinglePlaylist} from "./Pages";
 import { ProtectedRoute } from "./Routes/protected-route";
 import {Routes,Route} from "react-router-dom";
 import "./App.css";
@@ -14,9 +14,11 @@ function App() {
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/videos" element={<Videos/>}/>
         <Route path="/watch" element={<Watch/>}/>
+        <Route path="*" element={<h1 className="page_title center-text">There's Nothing Here!</h1>}/>
         <Route element={<ProtectedRoute/>}>
         <Route path="/liked" element={<Liked/>}/>
         <Route path="/playlist" element={<Playlist/>}/>
+        <Route path="/playlist/:playlistID" element={<SinglePlaylist/>}/>
         <Route path="/history" element={<History/>}/>
         <Route path="/watchlater" element={<WatchLater/>}/>
         </Route>
